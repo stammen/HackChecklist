f39e78a243cada676acde5134c17729b31790fb6
epsanchez <epsanchez@plainconcepts.com>
2017-04-26 13:30:44 +0200

    migrate from vsts to github and add some styles

---
+++ src/Microsoft.HackChecklist.UWP/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Microsoft.HackChecklist.UWP.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public bool Inverse { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var visibility = value != null && value is bool && (bool)value ? Visibility.Visible : Visibility.Collapsed;
+            return Inverse ? Invert(visibility) : visibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Visibility Invert(Visibility visibility)
+        {
+            return visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        }
+    }
+}
