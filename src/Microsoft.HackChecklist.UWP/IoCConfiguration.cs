8066bca03e5e4de6e5a416e0b193490995e009aa
Kenny <kreyes@plainconcepts.com>
2017-04-28 16:03:35 +0200

    Addidng license

--- src/Microsoft.HackChecklist.UWP/IoCConfiguration.cs
+++ src/Microsoft.HackChecklist.UWP/IoCConfiguration.cs
@@ -1,4 +1,15 @@
-﻿using Autofac;
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using Autofac;
 using Microsoft.HackChecklist.Services;
 using Microsoft.HackChecklist.Services.Contracts;
 using Microsoft.HackChecklist.UWP.Contracts;
