e1bef3d6cb350d0e1e8b1d8c2c70b3d94b0141f5
Kenny <kreyes@plainconcepts.com>
2017-04-28 12:02:40 +0200

    Including wrappers for VM

---
+++ src/Microsoft.HackChecklist.Models/Contracts/ISoftware.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.HackChecklist.Models.Enums;
+
+namespace Microsoft.HackChecklist.Models.Contracts
+{
+    public interface ISoftware
+    {
+        string Name { get; set; }
+        string AdditionalInformation { get; set; }
+        CheckType CheckType { get; set; }
+        string InstallationRegistryKey { get; set; }
+        string InstallationRegistryValue { get; set; }
+        string InstallationRegistryExpectedValue { get; set; }
+        bool IsOptional { get; set; }
+        string InstallationNotes { get; set; }
+        string Status { get; set; }
+        bool ActivateLoading { get; set; }
+    }
+}
---
+++ src/Microsoft.HackChecklist.UWP/ViewModels/RequirementViewModel.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.HackChecklist.Models;
+using Microsoft.HackChecklist.Models.Contracts;
+using Microsoft.HackChecklist.Models.Enums;
+using Microsoft.HackChecklist.UWP.ViewModels.Base;
+
+namespace Microsoft.HackChecklist.UWP.ViewModels
+{
+    public class RequirementViewModel : ViewModelBase, ISoftware
+    {
+        private Requirement _requirement;
+
+        public RequirementViewModel(Requirement requirement)
+        {
+            ModelObject = requirement;
+        }
+
+        public string Name
+        {
+            get => _requirement.Name;
+            set
+            {
+                _requirement.Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string AdditionalInformation
+        {
+            get => _requirement.AdditionalInformation;
+            set
+            {
+                _requirement.AdditionalInformation = value;
+                OnPropertyChanged(nameof(AdditionalInformation));
+            }
+        }
+
+        public CheckType CheckType
+        {
+            get => _requirement.CheckType;
+            set
+            {
+                _requirement.CheckType = value;
+                OnPropertyChanged(nameof(CheckType));
+            }
+        }
+
+        public string InstallationRegistryKey
+        {
+            get => _requirement.InstallationRegistryKey;
+            set
+            {
+                _requirement.InstallationRegistryKey = value;
+                OnPropertyChanged(nameof(InstallationRegistryKey));
+            }
+        }
+
+        public string InstallationRegistryValue
+        {
+            get => _requirement.InstallationRegistryValue;
+            set
+            {
+                _requirement.InstallationRegistryValue = value;
+                OnPropertyChanged(nameof(InstallationRegistryValue));
+            }
+        }
+
+        public string InstallationRegistryExpectedValue
+        {
+            get => _requirement.InstallationRegistryExpectedValue;
+            set
+            {
+                _requirement.InstallationRegistryExpectedValue = value;
+                OnPropertyChanged(nameof(InstallationRegistryExpectedValue));
+            }
+        }
+
+        public bool IsOptional
+        {
+            get => _requirement.IsOptional;
+            set
+            {
+                _requirement.IsOptional = value;
+                OnPropertyChanged(nameof(IsOptional));
+            }
+        }
+
+        public string InstallationNotes
+        {
+            get => _requirement.InstallationNotes;
+            set
+            {
+                _requirement.InstallationNotes = value;
+                OnPropertyChanged(nameof(InstallationNotes));
+            }
+        }
+
+        public string Status
+        {
+            get => _requirement.Status;
+            set
+            {
+                _requirement.Status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+        public bool ActivateLoading
+        {
+            get => _requirement.ActivateLoading;
+            set
+            {
+                _requirement.ActivateLoading = value;
+                OnPropertyChanged(nameof(ActivateLoading));
+            }
+        }
+
+        public ObservableCollection<Software> Modules { get; set; }
+
+        public Requirement ModelObject
+        {
+            get
+            {
+                _requirement.Modules = Modules.ToList();
+                return _requirement;
+            }
+            set
+            {
+                if (value == null) return;
+                _requirement = value;
+                if (_requirement.Modules != null)
+                {
+                    Modules = new ObservableCollection<Software>(_requirement.Modules);
+                }
+            }
+        }
+    }
+}
